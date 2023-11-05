7eff72350d300f806c1df1fd281fb2ed04ba692a
amirakame <amirakamel343@gmail.com>
2023-11-05 13:21:32 -0800

    chool

---
+++ web_dynamic/1-hbnb.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+	  let amenityIds = {};
+
+	  // Listen for changes on each input checkbox tag
+	//   $('input[type="checkbox"]').change(function () {
+	//       const amenityId = $(this).data('id');
+	//           const amenityName = $(this).data('name');
+	//               if (this.checked) {
+	//                     amenityIds[amenityId] = amenityName;
+	//                         } else {
+	//                               delete amenityIds[amenityId];
+	//                                   }
+	//
+	//                                       // Update the h4 tag inside the div Amenities with the list of Amenities checked
+	//                                           const amenitiesList = Object.values(amenityIds).join(', ');
+	//                                               $('div.Amenities h4').text(amenitiesList);
+	//                                                 });
+	//                                                 });
+	//
